762131889a84ca2fe0af233c1ae5492201671119
MarsWill1 <47376869+MarsWill1@users.noreply.github.com>
2022-12-29 23:14:37 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-uvfr8s
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-uvfr8s)
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'react-windows-ui/config/app-config.css';
+import "react-windows-ui/config/app-config.css";
+import "react-windows-ui/dist/react-windows-ui.min.css";
+import "react-windows-ui/icons/fonts/fonts.min.css";
+
